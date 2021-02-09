f51a0fd5158c3ff75fa85b3e5429fb61c0018c8e
Elisha Jobe <elishakjobe@gmail.com>
2021-02-09 03:45:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,9 +13,9 @@ return "You are not a teenager";
 
 function ageChecker(age) {
 if(age >= 13 && age <= 19) {
-console.log("You are a teenager!");
+return("You are a teenager!");
 } else if (age <= 12) {
-console.log("You are a kid");
+return("You are a kid");
 } else if(age >= 19) {
 return "You are a grownup";
 }
